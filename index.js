1dabd78a1cd1a313f5bc7975f9796e571b3a7c30
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-26 23:49:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var myFriendsNames = ["ashik", "salman", "jankar", "sharukh"];
+var myFriendsAges = [21, 22, 34, 45];
+var friendsProperty = [1000, 10000, 5000, 10000];
+var muriChanacurFee = [101, 137, 523, 422];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(myFriendsNames);
+// console.log(typeof myFriendsNames);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
