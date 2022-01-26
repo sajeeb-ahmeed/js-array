3c1bb21be660c55d55f18320d81a1f8f439ad95e
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-27 00:08:59 +0600

    array indexof

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var myFriendsNames = ["ashik", "salman", "jankar", "sharukh"];
+var myFriendsNames = ["ashik", "salman", "jhankar", "sharukh"];
 var myFriendsAges = [21, 22, 34, 45];
 var friendsProperty = [1000, 10000, 5000, 10000];
 var muriChanacurFee = [101, 137, 523, 422];
@@ -8,3 +8,9 @@ console.log(myFriendsNames);
 // console.log(typeof myFriendsNames);
 var vowelsCount = vowels.length;
 console.log(vowelsCount);
+
+//array-index
+var friendNameIndex = myFriendsNames.indexOf("jhankar");
+console.log(friendNameIndex);
+var index = vowels.indexOf('o');
+console.log(index);
