f7b461727d913e4cbe40bc47370bdbd4948ba27a
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-27 19:33:00 +0600

    rssult test normal

--- comparison.js
+++ comparison.js
@@ -11,34 +11,73 @@ var MyBudget = 110;
 // } else {
 //     console.log("Alu Borta dia vat den 1 plate")
 // }
-if (chikenPrice < MyBudget) {
-    console.log("Ami Muri Khamu Give me 1 pcs Chiken")
-} else {
-    console.log("Alu Borta dia vat den 1 plate")
-}
+// if (chikenPrice < MyBudget) {
+//     console.log("Ami Muri Khamu Give me 1 pcs Chiken")
+// } else {
+//     console.log("Alu Borta dia vat den 1 plate")
+// }
 
-var sojib = 1100;
-var shakib = 1050;
+// var sojib = 1100;
+// var shakib = 1050;
 
-if (sojib > shakib) {
-    console.log("Sojib Well Done")
-} else {
-    console.log(" Shakib well done")
-}
+// if (sojib > shakib) {
+//     console.log("Sojib Well Done")
+// } else {
+//     console.log(" Shakib well done")
+// }
 // multiple condition 
 var gotJob = true;
 var moneySaved = 2000;
 var haveHouse = true;
 var buyFlat = false;
 
-if ((gotJob == true && moneySaved > 300000) || haveHouse == false) {
-    console.log("biye kore felo");
+// if ((gotJob == true && moneySaved > 300000) || haveHouse == false) {
+//     console.log("biye kore felo");
 
+// } else {
+//     console.log("tor kopale biye nai....!!")
+// }
+// if (gotJob == true && haveHouse == true && moneySaved < 2200) {
+//     console.log("hey ami tumr ");
+// } else {
+//     console.log("tumi keda ami chini na ------- Like a selfish")
+// }
+// nested logic
+
+var alia = 86;
+var dalia = 90;
+var lalia = 75;
+var kalia = 95;
+var palia = 50;
+var grade = 95;
+
+if (alia > grade) {
+    console.log("Alia " + "= A+");
 } else {
-    console.log("tor kopale biye nai....!!")
+    console.log("Alia " + "= A-");
 }
-if (gotJob == true && haveHouse == true && moneySaved < 2200) {
-    console.log("hey ami tumr ");
+if (dalia > grade) {
+    console.log("Dalia" + "= A+");
 } else {
-    console.log("tumi keda ami chini na ------- Like a selfish")
+    console.log("Dalia " + "= A");
+}
+if (lalia > grade) {
+    console.log("Lailia " + " = A+ ");
+} else {
+    console.log("Lailia " + " = B");
+}
+if (kalia > grade) {
+    console.log("Kalia Fail Marse ");
+} else {
+    console.log("Kalia " + "= A+ ");
+}
+if (palia < grade) {
+    console.log("Palia ---> Sorry To say Tumi Fail");
+} else {
+    console.log('WOW GREAT RESULT')
+}
+if (grade < 100) {
+    console.log("YES, YOU CAN DO IT ")
+} else {
+    console.log("Programming Hobe nah toke diya")
 }
