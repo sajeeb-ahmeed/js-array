99e1b7f79852e8de514669559a9cd995b48d87bd
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-01-27 17:04:50 +0600

    if-else

--- comparison.js
+++ comparison.js
@@ -1,3 +1,27 @@
 console.log(7 > 9);
 console.log(9 < 10);
 console.log(9 == 9);
+
+
+
+var chikenPrice = 170;
+var MyBudget = 110;
+// if (chikenPrice > MyBudget) {
+//     console.log("Ami Muri Khamu Give me 1 pcs Chiken")
+// } else {
+//     console.log("Alu Borta dia vat den 1 plate")
+// }
+if (chikenPrice < MyBudget) {
+    console.log("Ami Muri Khamu Give me 1 pcs Chiken")
+} else {
+    console.log("Alu Borta dia vat den 1 plate")
+}
+
+var sojib = 1100;
+var shakib = 1050;
+
+if (sojib > shakib) {
+    console.log("Sojib Well Done")
+} else {
+    console.log(" Shakib well done")
+}
